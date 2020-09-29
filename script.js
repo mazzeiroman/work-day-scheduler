551f309847232280c7799f9188fbd8c367f9a941
Roman Mazzei <mazzeiroman@gmail.com>
2020-09-29 14:08:25 -0400

    converted my dayHours array into time, to comparae it to the actual time, to asign the past present or future classes to my input

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
     $("#currentDay").html(moment().format('dddd, '+ "MMM Do "));
 
 var container = $(".container");
-var dayLength = 8;
-var dayHours =["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
+var dayHours =["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+var hour = moment().format('h A');
+console.log(hour)
 
-for (var i = 0; i<= dayLength; i++) {
+
+for (var i = 0; i< dayHours.length; i++) {
     var newDivRow = $("<div>" +"</div>");
     newDivRow.addClass("row");
     container.append(newDivRow);
@@ -19,7 +21,7 @@ for (var i = 0; i<= dayLength; i++) {
   newDivCol2.append(spanCol2);
 
   var newDivCol9 =  $("<div>" + "</div>");
-  newDivCol9.addClass("col-md-9 past present future");
+  newDivCol9.addClass("col-md-9 ");
   newDivRow.append(newDivCol9);
 
   var newInput9 = $("<input>");
@@ -29,9 +31,20 @@ for (var i = 0; i<= dayLength; i++) {
   var newDivCol1 =  $("<div>" + "</div>");
   newDivCol1.addClass("col-md-1");
   newDivRow.append(newDivCol1);
-    // container.append(newDivRow);
-//    var newRow= container.html("<div>" + "</div>");
-//    newRow
 
+  var icon = $("<i>" + "</i>");
+  icon.addClass("far fa-2x fa-save");
+  newDivCol1.append(icon);
+
+  if (hour == dayHours[i]) {
+    newDivCol9.addClass("present")}
+ else if (hour < dayHours[i] ){
+    newDivCol9.addClass("future") 
+ }
+ else if (hour > dayHours[i]) {
+    newDivCol9.addClass("past")
+ }
+    
+ console.log(moment(dayHours[i]));
+  
 }
-console.log(newDivRow)
